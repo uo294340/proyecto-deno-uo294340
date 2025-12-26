5577e139495dfe72adc1e41b990f458a9fb36dbf
Maria <uo294340@uniovi.es>
2025-12-26 13:27:47 +0100

    cambios en el main ts para error de Linter

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
 const port = 8080;
 
-const handler = (request: Request): Response => {
+const handler = (_request: Request): Response => {
   const body = `¡Hola! Soy el alumno del UO294340. Este es mi stack Deno.`;
   return new Response(body, { status: 200 });
 };
